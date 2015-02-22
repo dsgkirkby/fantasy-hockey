50f2b5a3c6e9fc53eddbd405e7b0334e89caf97f
Eric <esecules@gmail.com>
2015-02-21 21:05:15 -0800

    added skeleton for post login screen

--- web/new_user.php
+++ web/new_user.php
@@ -15,59 +15,60 @@ and open the template in the editor.
         <script src="bootstrapp-3.3.2-dist/js/bbootstrap.min.js"></script>
     </head>
     <body>
-      <form class="form-horizontal">
-<fieldset>
+        <div class="container">
+            <form class="form-horizontal">
+                <fieldset>
 
-<!-- Form Name -->
-<legend>Register</legend>
+                    <!-- Form Name -->
+                    <legend>Register</legend>
 
-<!-- Text input-->
-<div class="control-group">
-  <label class="control-label" for="uname">Username</label>
-  <div class="controls">
-    <input id="uname" name="uname" type="text" placeholder="" class="input-xlarge" required="">
-    
-  </div>
-</div>
+                    <!-- Text input-->
+                    <div class="control-group">
+                        <label class="control-label" for="uname">Username</label>
+                        <div class="controls">
+                            <input id="uname" name="uname" type="text" placeholder="" class="input-xlarge" required="">
 
-<!-- Text input-->
-<div class="control-group">
-  <label class="control-label" for="email">Email Address</label>
-  <div class="controls">
-    <input id="email" name="email" type="text" placeholder="" class="input-xlarge" required="">
-    
-  </div>
-</div>
+                        </div>
+                    </div>
 
-<!-- Password input-->
-<div class="control-group">
-  <label class="control-label" for="passwd">Password</label>
-  <div class="controls">
-    <input id="passwd" name="passwd" type="password" placeholder="****************" class="input-xlarge" required="">
-    
-  </div>
-</div>
+                    <!-- Text input-->
+                    <div class="control-group">
+                        <label class="control-label" for="email">Email Address</label>
+                        <div class="controls">
+                            <input id="email" name="email" type="text" placeholder="" class="input-xlarge" required="">
 
-<!-- Password input-->
-<div class="control-group">
-  <label class="control-label" for="conf_passwd">Confirm Password</label>
-  <div class="controls">
-    <input id="conf_passwd" name="conf_passwd" type="password" placeholder="****************" class="input-xlarge" required="">
-    
-  </div>
-</div>
+                        </div>
+                    </div>
 
-<!-- Button -->
-<div class="control-group">
-  <label class="control-label" for="submit"></label>
-  <div class="controls">
-    <button id="submit" name="submit" class="btn btn-primary">Submit</button>
-  </div>
-</div>
+                    <!-- Password input-->
+                    <div class="control-group">
+                        <label class="control-label" for="passwd">Password</label>
+                        <div class="controls">
+                            <input id="passwd" name="passwd" type="password" placeholder="****************" class="input-xlarge" required="">
 
-</fieldset>
-</form>
+                        </div>
+                    </div>
 
+                    <!-- Password input-->
+                    <div class="control-group">
+                        <label class="control-label" for="conf_passwd">Confirm Password</label>
+                        <div class="controls">
+                            <input id="conf_passwd" name="conf_passwd" type="password" placeholder="****************" class="input-xlarge" required="">
+
+                        </div>
+                    </div>
+
+                    <!-- Button -->
+                    <div class="control-group">
+                        <label class="control-label" for="submit"></label>
+                        <div class="controls">
+                            <button id="submit" name="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
         <?php
         // put your code here
         ?>
