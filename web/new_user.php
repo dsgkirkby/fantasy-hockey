ab2ca2b6d407635aab9c8bbb1152ae9102721d99
p-mcgowan <mickeymcgowan@shaw.ca>
2015-02-24 11:54:19 -0800

    testing

--- web/new_user.php
+++ web/new_user.php
@@ -15,62 +15,83 @@ and open the template in the editor.
         <script src="bootstrapp-3.3.2-dist/js/bbootstrap.min.js"></script>
     </head>
     <body>
-        <div class="container">
-            <form class="form-horizontal">
-                <fieldset>
+      <form class="form-horizontal">
+<fieldset>
 
-                    <!-- Form Name -->
-                    <legend>Register</legend>
+<!-- Form Name -->
+<legend>Register</legend>
 
-                    <!-- Text input-->
-                    <div class="control-group">
-                        <label class="control-label" for="uname">Username</label>
-                        <div class="controls">
-                            <input id="uname" name="uname" type="text" placeholder="" class="input-xlarge" required="">
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="uname">Username</label>
+  <div class="controls">
+    <input id="uname" name="uname" type="text" placeholder="" class="input-xlarge" required="">
+    
+  </div>
+</div>
 
-                        </div>
-                    </div>
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="email">Email Address</label>
+  <div class="controls">
+    <input id="email" name="email" type="text" placeholder="" class="input-xlarge" required="">
+    
+  </div>
+</div>
 
-                    <!-- Text input-->
-                    <div class="control-group">
-                        <label class="control-label" for="email">Email Address</label>
-                        <div class="controls">
-                            <input id="email" name="email" type="text" placeholder="" class="input-xlarge" required="">
+<!-- Password input-->
+<div class="control-group">
+  <label class="control-label" for="passwd">Password</label>
+  <div class="controls">
+    <input id="passwd" name="passwd" type="password" placeholder="****************" class="input-xlarge" required="">
+    
+  </div>
+</div>
 
-                        </div>
-                    </div>
+<!-- Password input-->
+<div class="control-group">
+  <label class="control-label" for="conf_passwd">Confirm Password</label>
+  <div class="controls">
+    <input id="conf_passwd" name="conf_passwd" type="password" placeholder="****************" class="input-xlarge" required="">
+    
+  </div>
+</div>
 
-                    <!-- Password input-->
-                    <div class="control-group">
-                        <label class="control-label" for="passwd">Password</label>
-                        <div class="controls">
-                            <input id="passwd" name="passwd" type="password" placeholder="****************" class="input-xlarge" required="">
+<!-- Button -->
+<div class="control-group">
+  <label class="control-label" for="submit"></label>
+  <div class="controls">
+    <button id="submit" name="submit" class="btn btn-primary" onclick=asserValidInput();>Submit</button>
+  </div>
+</div>
 
-                        </div>
-                    </div>
+</fieldset>
+</form>
 
-                    <!-- Password input-->
-                    <div class="control-group">
-                        <label class="control-label" for="conf_passwd">Confirm Password</label>
-                        <div class="controls">
-                            <input id="conf_passwd" name="conf_passwd" type="password" placeholder="****************" class="input-xlarge" required="">
-
-                        </div>
-                    </div>
-
-                    <!-- Button -->
-                    <div class="control-group">
-                        <label class="control-label" for="submit"></label>
-                        <div class="controls">
-                            <button id="submit" name="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-
-                </fieldset>
-            </form>
-        </div>
         <?php
-        // put your code here
+        function assertValidInput(){
+        // Create an empty file if none exists
+        if (!file_exists("usernames.txt")) {
+          $usernames = fopen("usernames.txt", "w");
+          fclose($usernames);
+        }
+
+        // Open username file
+        $usernames = fopen("usernames.txt", "r");
+
+        // Parse usernames - probably a faster way to do this, but not sure
+        while(!feof($usernames)) {
+          if !(strcmp(fgets($usernames), uname)) {
+            echo "Username exists.";
+            break;
+            // Report error and reload page
+          }
+        }
+        
+        //create new user and append to file.
+        fwrite($usernames, $uname);
+        // Report sucees and return to login (or straight in)
+      }
         ?>
     </body>
 </html>
