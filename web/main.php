10e1b5a5f5758c3f7d00c8d10edd78eb09d7166c
Dylan <dylan.pickel@gmail.com>
2015-02-25 14:11:31 -0800

    Updated for new bootstrap folder name

--- web/main.php
+++ web/main.php
@@ -9,10 +9,10 @@ and open the template in the editor.
         <title>Home</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
         <!-- Optional theme -->
-        <link rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
         <script src="bootstrapp-3.3.2-dist/js/bbootstrap.min.js"></script>
     </head>
     <body>
