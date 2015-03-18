23b0d304608326535e99e896d791e27765d7c670
Dylan <dylan.pickel@gmail.com>
2015-03-18 15:13:32 -0700

    Renamed for consistency

--- web/viewleague.php
+++ web/viewleague.php
@@ -32,30 +32,17 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="">Dobber Fantasy</a>
+                    <a class="navbar-brand">Dobber Fantasy</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="">Home</a></li>
-                        <li><a href="">Rankings</a></li>
-                        <li class="dropdown">
-                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">My Teams<span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="">Action</a></li>
-                                <li><a href="">Another action</a></li>
-                                <li><a href="">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="">Separated link</a></li>
-                                <li><a href="">One more separated link</a></li>
-                            </ul>
-                        </li>
+                        <li><a href="main.php">Home</a></li>
                     </ul>
 		    <ul class="nav navbar-nav navbar-right">
 			<li><a href="logout.php">Logout</a></li>
 		    </ul>
-                </div><!--/.nav-collapse -->
-            </div><!--/.container-fluid -->
+                </div>
+            </div>
         </nav>
         <div class="container">
             <table class="table table-bordered">
