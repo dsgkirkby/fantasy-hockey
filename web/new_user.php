04a389c88563e938283d0ac3a24ee12d7bfd1a0c
p-mcgowan <mickeymcgowan@shaw.ca>
2015-03-24 11:02:54 -0700

    redundant

--- web/new_user.php
+++ web/new_user.php
@@ -17,8 +17,6 @@ and open the template in the editor.
     </head>
     <body>
         <div class="container">
-            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
             <!-- Include all compiled plugins (below), or include individual files as needed -->
             <script src="js/bootstrap.min.js"></script>
