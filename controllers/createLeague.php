a8ca7e8e0bb1ccde7f3c580619d4f54e810f0a89
Dylan <dylan.pickel@gmail.com>
2015-03-23 17:35:01 -0700

    Creating leagues now also creates the manager relationship

--- controllers/createLeague.php
+++ controllers/createLeague.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once('../library/userVerification.php');
+session_start();
+
 $maxSize = $_GET["maxSize"];
 $name = $_GET["leagueName"];
 
@@ -14,13 +17,22 @@ if (mysqli_select_db($con, "dobber") == FALSE) {
     exit('DB select failed!');
 }
 
-$query = "INSERT INTO f_leagues (name, max_size, date_created) values "
-. "(\"" . $name . "\"," . $maxSize . ",\"" . date("Y-m-d") . "\")";
+$leagueInsert = "INSERT INTO f_leagues (name, max_size, date_created) values "
+	. "(\"" . $name . "\"," . $maxSize . ",\"" . date("Y-m-d") . "\")";
 
-$result = mysqli_query($con, $query);
+$result = mysqli_query($con, $leagueInsert);
+
+$leagueID = mysqli_insert_id($con);
+
+$managerInsert = "INSERT INTO manages (username, leagueID) values "
+	. "(\"" . getUsername() . "\", " . $leagueID . ")";
+
+error_log($managerInsert);
+
+mysqli_query($con, $managerInsert);
 
 if ($result) {
-    header("location: ../web/viewLeague.php?leagueID=" . mysqli_insert_id($con), true, 303);
+    header("location: ../web/viewLeague.php?leagueID=" . $leagueID, true, 303);
 } else {
     header("location: viewLeagues.php?error=true", true, 303);
 }
