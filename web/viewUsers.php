7e186f9f19dd3a092b4acababb1e06717ee810c8
p-mcgowan <mickeymcgowan@shaw.ca>
2015-03-26 16:07:57 -0700

    indentation conflict

--- web/viewUsers.php
+++ web/viewUsers.php
@@ -55,7 +55,6 @@ and open the template in the editor.
 		</div><!-- /.modal-content -->
 	  </div><!-- /.modal-dialog -->
 	</div><!-- /.modal -->
-<<<<<<< HEAD
 		<nav class="navbar navbar-default">
 			<div class="container-fluid">
 				<div class="navbar-header">
@@ -122,61 +121,4 @@ and open the template in the editor.
 				?>
 			</table>
 	</body>
-=======
-	<nav class="navbar navbar-default">
-	    <div class="container-fluid">
-		<div class="navbar-header">
-		    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		    </button>
-		    <a class="navbar-brand">Dobber Fantasy</a>
-		</div>
-		<div id="navbar" class="navbar-collapse collapse">
-		    <ul class="nav navbar-nav">
-			<li><a href="main.php">Home</a></li>
-			<li><a href="viewLeagues.php">Leagues</a></li>
-			<li><a href="viewPlayers.php">Players</a></li>
-			<li class="active"><a href="admin.php">Admin Tools</a></li>
-		    </ul>
-		    <ul class="nav navbar-nav navbar-right">
-			<li><a href="logout.php">Logout</a></li>
-		    </ul>
-		</div><!--/.nav-collapse -->
-	    </div><!--/.container-fluid -->
-	</nav>
-	<div class="container">
-	    <h2>All Users</h2>
-	    <table class="table table-bordered">
-		<tr>
-		    <th>Username</th>
-		    <th>Password</th>
-		    <th>Email Address</th>
-		    <th>Administrator</th>
-		    <th>Actions</th>
-		</tr>
-		<?php
-		$users = user::getUsers();
-		foreach($users as $user) {
-		    echo "<tr>"
-		    . "<td><a href=\"viewLeagues.php?username=" . $user["username"] . "\">" . $user["username"] . "</a></td>"
-		    . "<td>" . $user["password"] . "</td>"
-		    . "<td>" . $user["email"] . "</td>"
-		    . "<td>" . ($user["is_admin"] ? "Yes" : "No") . "</td>"
-		    . "<td><a class=\"btn btn-xs btn-primary\" href=\"\" onclick=startEdit(\"" 
-		    . $user["username"] . "\",\"" 
-		    . $user["password"] . "\",\"" 
-		    . $user["email"] . "\"," 
-		    . $user["is_admin"] 
-		    . ") data-toggle=\"modal\" data-target=\"#editModal\">Edit</a>"
-		    . " <a class=\"btn btn-xs btn-warning\" href=\"\" onclick=deleteUser(\"" . $user["username"] . "\")>Delete</a></td>"
-		    . "</tr>";
-		}
-		?>
-	    </table>
-	</div>
-    </body>
->>>>>>> 56360a56b525f2f6a5fab7eda8221a4d1361fd04
 </html>
