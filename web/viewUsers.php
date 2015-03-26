97ce6d440c2a398b89d3cdef1e9bf34edcba5ce4
Dylan <dylan.pickel@gmail.com>
2015-03-26 14:55:02 -0700

    Fixed indentation

--- web/viewUsers.php
+++ web/viewUsers.php
@@ -12,12 +12,12 @@ and open the template in the editor.
 	dieIfNotAdmin();
 	?>
 	<head>
-		<meta charset="UTF-8">
-		<title>Users View</title>
-		<script src="jquery-2.1.3.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="editUser.js"></script>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	    <meta charset="UTF-8">
+	    <title>Users View</title>
+	    <script src="jquery-2.1.3.min.js"></script>
+	    <script src="bootstrap/js/bootstrap.min.js"></script>
+	    <script src="editUser.js"></script>
+	    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	</head>
 	<body>
 	<div class="modal fade" id="editModal">
@@ -56,58 +56,59 @@ and open the template in the editor.
 		</div><!-- /.modal-content -->
 	  </div><!-- /.modal-dialog -->
 	</div><!-- /.modal -->
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand">Dobber Fantasy</a>
-				</div>
-				<div id="navbar" class="navbar-collapse collapse">
-					<ul class="nav navbar-nav">
-						<li><a href="main.php">Home</a></li>
-						<li><a href="viewLeagues.php">Leagues</a></li>
-						<li><a href="viewPlayers.php">Players</a></li>
+	<nav class="navbar navbar-default">
+	    <div class="container-fluid">
+		<div class="navbar-header">
+		    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		    </button>
+		    <a class="navbar-brand">Dobber Fantasy</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+		    <ul class="nav navbar-nav">
+			<li><a href="main.php">Home</a></li>
+			<li><a href="viewLeagues.php">Leagues</a></li>
+			<li><a href="viewPlayers.php">Players</a></li>
 			<li class="active"><a href="admin.php">Admin Tools</a></li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right">
 			<li><a href="logout.php">Logout</a></li>
-			</ul>
-				</div><!--/.nav-collapse -->
-			</div><!--/.container-fluid -->
-		</nav>
-		<div class="container">
-		<h2>All Users</h2>
-			<table class="table table-bordered">
-				<tr>
-					<th>Username</th>
-					<th>Password</th>
-					<th>Email Address</th>
-			<th>Administrator</th>
-			<th>Actions</th>
-				</tr>
-				<?php
-				$users = user::getUsers();
-				foreach($users as $user) {
-					echo "<tr>"
-					. "<td><a href=\"viewLeagues.php?username=" . $user["username"] . "\">" . $user["username"] . "</a></td>"
-					. "<td>" . $user["password"] . "</td>"
-					. "<td>" . $user["email"] . "</td>"
-					. "<td>" . ($user["is_admin"] ? "Yes" : "No") . "</td>"
-					. "<td><a class=\"btn btn-xs btn-primary\" href=\"\" onclick=startEdit(\"" 
-						. $user["username"] . "\",\"" 
-						. $user["password"] . "\",\"" 
-						. $user["email"] . "\"," 
-						. $user["is_admin"] 
-						. ") data-toggle=\"modal\" data-target=\"#editModal\">Edit</a>"
-					. " <a class=\"btn btn-xs btn-warning\" href=\"\" onclick=deleteUser(\"" . $user["username"] . "\")>Delete</a></td>"
-					. "</tr>";
-				}
-				?>
-			</table>
-	</body>
+		    </ul>
+		</div><!--/.nav-collapse -->
+	    </div><!--/.container-fluid -->
+	</nav>
+	<div class="container">
+	    <h2>All Users</h2>
+	    <table class="table table-bordered">
+		<tr>
+		    <th>Username</th>
+		    <th>Password</th>
+		    <th>Email Address</th>
+		    <th>Administrator</th>
+		    <th>Actions</th>
+		</tr>
+		<?php
+		$users = user::getUsers();
+		foreach($users as $user) {
+		    echo "<tr>"
+		    . "<td><a href=\"viewLeagues.php?username=" . $user["username"] . "\">" . $user["username"] . "</a></td>"
+		    . "<td>" . $user["password"] . "</td>"
+		    . "<td>" . $user["email"] . "</td>"
+		    . "<td>" . ($user["is_admin"] ? "Yes" : "No") . "</td>"
+		    . "<td><a class=\"btn btn-xs btn-primary\" href=\"\" onclick=startEdit(\"" 
+		    . $user["username"] . "\",\"" 
+		    . $user["password"] . "\",\"" 
+		    . $user["email"] . "\"," 
+		    . $user["is_admin"] 
+		    . ") data-toggle=\"modal\" data-target=\"#editModal\">Edit</a>"
+		    . " <a class=\"btn btn-xs btn-warning\" href=\"\" onclick=deleteUser(\"" . $user["username"] . "\")>Delete</a></td>"
+		    . "</tr>";
+		}
+		?>
+	    </table>
+	</div>
+    </body>
 </html>
