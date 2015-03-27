0d249fbfc230c39a8902bf2da6481b889dcb1a2a
Eric Secules <esecules@gmail.com>
2015-03-26 21:56:25 -0700

    added add/remove player controllers

---
+++ controllers/addPlayer.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('../library/conn.php');
+
+
+$playerID = $_GET["playerID"];
+$teamID = $_GET["teamID"];
+
+$con = conn::getDB();
+$playerAssignInsert = "INSERT INTO player_assignments (playerID, teamID) values "
+        . "(" . $playerID . ",\"" . $teamID . "\")";
+
+$result = mysqli_query($con, $playerAssignInsert);
+
+error_log($playerAssignInsert);
+
+if ($result) {
+    header("location: ../web/viewTeam.php", true, 303);
+} else {
+    header("location: ../web/viewTeam.php?error=true", true, 303);
+}
+
