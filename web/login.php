2f1af95814490278ea142d031b702b48d7f9746e
p-mcgowan <mickeymcgowan@shaw.ca>
2015-03-26 22:22:05 -0700

    retab

--- web/login.php
+++ web/login.php
@@ -17,7 +17,7 @@
 		<meta charset="UTF-8">
 		<title>Dobber Login</title>
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<script src="jquery-2.1.3.min.js"></script>
+		<script src="jquery-2.1.3.min.js"></script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
 	</head>
 
@@ -43,30 +43,30 @@
 			</div>
 			<div class="container">
 				<div class="col-sm-4">
-			<form action="verifyUser.php" class="form-horizontal <?php if ($error) { echo "has-error"; } ?>">
+					<form action="verifyUser.php" class="form-horizontal <?php if ($error) { echo "has-error"; } ?>">
 						<fieldset>
 							<legend>Login</legend>
 							<div class="form-group">
 								<label class="control-label" for="username">Username</label>
-				<input class="form-control input-xlarge" id="username"
-					   name="username" type="text" placeholder="" required="">
+								<input class="form-control input-xlarge" id="username"
+								name="username" type="text" placeholder="" required="">
 							</div>
 							<div class="form-group">
 								<label class="control-label" for="password">Password</label>
-				<input class="form-control input-xlarge" id="password" 
-					   name="password" type="password" placeholder="*********" required="">
+								<input class="form-control input-xlarge" id="password" 
+								name="password" type="password" placeholder="*********" required="">
 							</div>
 							<div class="form-group">
-				<button type="submit" class="btn btn-primary">Login</button>
-				<a href="new_user.php">Register</a>
+								<button type="submit" class="btn btn-primary">Login</button>
+								<a href="new_user.php">Register</a>
 							</div>
-				<div>
-				<?php
-					//if ($error) {
-					//echo "<font color=\"#a94442\">Invalid Login</font>";
-					//}
-				?>
-				</div>
+							<div>
+							<?php
+							//if ($error) {
+							//echo "<font color=\"#a94442\">Invalid Login</font>";
+							//}
+							?>
+							</div>
 						</fieldset>
 					</form>
 				</div>
